39f995727201caa71e2aeea995f40f65ebe4785f
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-02-23 01:59:35 +0530

    modified

--- 01_ReistrationForm/index.js
+++ 01_ReistrationForm/index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv")
@@ -6,21 +6,63 @@ const dotenv = require("dotenv")
 const app = express ();
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.hevhgcf.mongodb.net/registrationFormDB`,{
+    // useNewUrlParser : true,
+    // useUnifiedTopology : true,
+});
+
+const registrationSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String
+});
+
+const Registration = mongoose.model("Registration",registrationSchema);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/index.html")
-})
+});
 
-app.post("/reister",(req,res)=>{
+app.post("/register",async(req,res)=>{
     try{
         const{name,email,password} = req.body;
-    }
-    catch{
 
+        const existingUser = await Registration.findOne({email:email});
+        if(!existingUser){
+            const registrationData = new Registration({
+                name,
+                email,
+                password
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        }
+        else{
+            console.log("User Already Exists");
+            res.redirect("/error");
+        }
     }
-})
+    catch (error){
+        console.log(error);
+        res.redirect("/error");
+    }
+});
+
+app.get("/success",(req,res)=>{
+    res.sendFile(__dirname+"/success.html");
+});
+
+app.get("/error",(req,res)=>{
+    res.sendFile(__dirname+"/error.html");
+});
 
 app.listen(port,()=>{
     console.log(`server is runnin on port ${port}`)
-})
+});
