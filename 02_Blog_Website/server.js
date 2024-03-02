673976e4ca578a2300ed89251aecc4f6bf395c89
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-03-03 03:33:35 +0530

    modified

--- 02_Blog_Website/server.js
+++ 02_Blog_Website/server.js
@@ -2,17 +2,17 @@ const express = require('express')
 const articleRouter = require('./routes/articles')
 const app = express()
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.use('/articles',articleRouter)
+app.use('/articles', articleRouter)
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     const articles = [{
-        title:'Test Article',
-        createdAt:Date.now(),
+        title: 'Test Article',
+        createdAt: Date.now(),
         description: 'Test description'
     }]
-    res.render('index',{articles: articles})
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000)
