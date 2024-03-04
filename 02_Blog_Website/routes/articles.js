92fd6955f307f92d9006ba8005969d92e745bea6
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-03-05 02:44:21 +0530

    modified

--- 02_Blog_Website/routes/articles.js
+++ 02_Blog_Website/routes/articles.js
@@ -4,27 +4,32 @@ const Article = require('./../models/article')
 const article = require('./../models/article')
 
 router.get('/new', (req, res) => {
-    res.render('articles/new',{article : new Article()})
+    res.render('articles/new', { article: new Article() })
 })
 
-router.get('/:id',async(req,res)=>{
-    const article = await Article.findById(req.params.id)
-    if(article == null) res.redirect('/')
-    res.render('articles/show',{article: article})
+router.get('/:slug', async (req, res) => {
+    const article = await Article.findOne({ slug: req.params.slug })
+    if (article == null) res.redirect('/')
+    res.render('articles/show', { article: article })
 })
 
-router.post('/',async(req,res)=>{
+router.post('/', async (req, res) => {
     let article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown
     })
-    try{
-        article=await article.save()
-        res.redirect(`/articles/${article.id}`)
-    }catch(e){
-        res.render('articles/new',{article: article})
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+        res.render('articles/new', { article: article })
     }
+})
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
 })
 
 module.exports = router
--- 02_Blog_Website/server.js
+++ 02_Blog_Website/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 dotenv.config();
@@ -15,26 +17,15 @@ mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.n00jk0m.mongodb
 
 app.set('view engine', 'ejs')
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article2',
-        createdAt: new Date(),
-        description: 'Test description 2'
-    }
-]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/index', { articles: articles })
 })
-
 // app.listen(5000)
 app.use('/articles', articleRouter)
-
 app.listen(port, () => {
     console.log(`server is runnin on port ${port}`)
 })
