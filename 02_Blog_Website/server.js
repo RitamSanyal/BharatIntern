c4cdaae7cee1ecb629597fa6dc3c002796e2cfee
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-03-04 15:50:47 +0530

    new file and modified

--- 02_Blog_Website/server.js
+++ 02_Blog_Website/server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const username = process.env.MONGODB_USERNAME
+const password = process.env.MONGODB_PASSWORD
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.n00jk0m.mongodb.net/BlogDB`)
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     const articles = [{
@@ -13,12 +24,17 @@ app.get('/', (req, res) => {
         description: 'Test description'
     },
     {
-        title: 'Test Article2', 
+        title: 'Test Article2',
         createdAt: new Date(),
         description: 'Test description 2'
     }
-    ]
+]
     res.render('articles/index', { articles: articles })
 })
 
-app.listen(5000)
+// app.listen(5000)
+app.use('/articles', articleRouter)
+
+app.listen(port, () => {
+    console.log(`server is runnin on port ${port}`)
+})
